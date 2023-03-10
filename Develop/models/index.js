b2b1b4c5a6ffb8118719b52e664dcf26b10f2971
Safiya Shrader <saf1ya.c0d3z@gmail.com>
2023-03-10 07:05:55 -0800

    add category.hasone/product.belongsto

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,7 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasOne(Product, {
+  foreignKey: 'category_id',
+  // TODO: Add a comment describing the functionality of this property
+  onDelete: 'CASCADE',
+});
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
