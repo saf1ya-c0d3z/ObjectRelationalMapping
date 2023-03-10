5fa096ba3f9ae3436a7fd54c23f9cc8bce6dca91
Safiya Shrader <saf1ya.c0d3z@gmail.com>
2023-03-10 10:33:59 -0800

    finish category-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,7 +5,7 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll();
+    const userData = await Category.findAll();
     res.status(200).json(userData);
   } catch (err) {
     res.status(404).json(err);
@@ -15,11 +15,18 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
-  const userData = await Category.findByPk(req.params.id).catch((err) =>
-    res.status(200).json(err));
-   } catch (err) {
-  res.status(404).json(userData)
-   ;
+    const readerData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
   }
   // be sure to include its associated Products
 });
@@ -28,7 +35,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const readerData = await Category.create(req.body);
-    res.status(200).json(readerData);
+    res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -38,7 +45,23 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const readerData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete a category by its `id` value
 });
 
